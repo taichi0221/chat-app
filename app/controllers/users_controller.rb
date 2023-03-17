1cd13eeba0eb36cd7b31780287c39a19a706eeb8
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-03-17 22:32:46 +0900

    バリデーションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
+
   def edit
   end
 
   def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
+    current_user.update(user_params)
   end
 
   private
@@ -15,5 +12,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
-
 end
